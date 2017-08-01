6496eb0ef3c6d6ebd2cfc6a3ca0057f376a84a92
robogao <wjm333@gmail.com>
2017-07-31 23:21:39 -0400

    Update Protractor.h

--- Protractor.h
+++ Protractor.h
@@ -27,7 +27,7 @@
   proximity sensor.
 
   For a complete tutorial on wiring up and using the Protractor go to:
-    http://www.robogao.com/Protractor
+    http://www.will-moore.com/protractor/ProtractorAngleProximitySensor_UserGuide.pdf
 	
   ############################################################################
 */
